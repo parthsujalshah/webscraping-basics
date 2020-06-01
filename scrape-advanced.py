a5a00d4b64b6abcb653d6c1963f9bee8e4779165
parthsujalshah <parthsujalshah@gmail.com>
2020-06-01 10:52:39 +0530

    addede try except for none types

--- scrape-advanced.py
+++ scrape-advanced.py
@@ -16,13 +16,17 @@ for article in soup.find_all('article'):
   summary = article.find('div', class_='entry-content').p.text
   print(summary)
 
-  # vid_src = article.find('iframe', class_='youtube-player')
-  vid_src = article.find('iframe', class_='youtube-player')['src']
-  # print(vid_src)
+  try:
+    # vid_src = article.find('iframe', class_='youtube-player')
+    vid_src = article.find('iframe', class_='youtube-player')['src']
+    # print(vid_src)
 
-  vid_id = vid_src.split('/')[4].split('?')[0]
-  # print(vid_id) #this is the youtube video id
+    vid_id = vid_src.split('/')[4].split('?')[0]
+    # print(vid_id) #this is the youtube video id
 
-  yt_link = f'https://youtube.com/watch?v={vid_id}'
+    yt_link = f'https://youtube.com/watch?v={vid_id}'
+  except Exception as e:
+    yt_link = None
+
   print(yt_link)
   print()
